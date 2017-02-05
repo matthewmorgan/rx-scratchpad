4455c2f3459812bff5acc9be5283ab7a0a5d4823
Matthew Morgan <matt@dulcetsoftware.com>
2017-02-05 08:22:03 -0500

    Use retry and takeWhile

--- main.js
+++ main.js
@@ -11,14 +11,29 @@ function load(url) {
   return Observable.create(observer => {
     let xhr = new XMLHttpRequest();
     xhr.addEventListener("load", () => {
-      let data = JSON.parse(xhr.responseText);
-      observer.next(data);
-      observer.complete();
+      if(xhr.status === 200){
+        let data = JSON.parse(xhr.responseText);
+        observer.next(data);
+        observer.complete();
+      } else {
+        observer.error(xhr.statusText);
+      }
     });
 
     xhr.open("GET", url);
     xhr.send();
-  });
+  }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
+}
+
+function retryStrategy({attempts = 4, delay = 1000}){
+  return function(errors){
+    return errors.scan((acc, value) => {
+      console.log(acc, value);
+      return acc + 1;
+    }, 0)
+        .takeWhile(acc => acc < attempts)
+        .delay(delay);
+  }
 }
 
 function renderMovies(movies) {
