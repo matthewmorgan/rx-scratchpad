2cbd2b99ecd68e22d47ea849a3b80525ac86d3e1
Matthew Morgan <matt@dulcetsoftware.com>
2017-02-05 12:25:15 -0500

    Refactor to throw error after all retries fail

--- loader.js
+++ loader.js
@@ -5,7 +5,7 @@ export function load(url) {
   return Observable.create(observer => {
     let xhr = new XMLHttpRequest();
     xhr.addEventListener("load", () => {
-      if(xhr.status === 200){
+      if (xhr.status === 200) {
         let data = JSON.parse(xhr.responseText);
         observer.next(data);
         observer.complete();
@@ -19,26 +19,23 @@ export function load(url) {
   }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
 }
 
-export function loadWithFetch(url){
-  return Observable.defer(()=> {
+export function loadWithFetch(url) {
+  return Observable.defer(() => {
     return Observable.fromPromise(
-            fetch(url).then(r => {
-                  if (r.statusCode === 200) {
-                    return r.json();
-                  } else {
-                    return Promise.reject(r);
-                  }
-                })).retryWhen(retryStrategy());
-      });
+        fetch(url).then(r => r.statusCode === 200) ? r.json() : Promise.reject(r))
+  }).retryWhen(retryStrategy());
 }
 
-function retryStrategy({attempts = 4, delay = 1000} = {}){
-  return function(errors){
+function retryStrategy({attempts = 4, delay = 1000} = {}) {
+  return function (errors) {
     return errors.scan((acc, value) => {
-      console.log(acc, value);
-      return acc + 1;
+      acc++;
+      if (acc < attempts) {
+        return acc;
+      } else {
+        throw new Error(value);
+      }
     }, 0)
-        .takeWhile(acc => acc < attempts)
         .delay(delay);
   }
 }
