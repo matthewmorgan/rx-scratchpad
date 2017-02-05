2af73c6c0ddf3327a36db3507fc9a4bcd5cca370
Matthew Morgan <matt@dulcetsoftware.com>
2017-02-05 07:38:25 -0500

    Create mouse tracking circle

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 import {Observable} from "rxjs";
 
 
-const source = Observable.fromEvent(document, 'mousemove');
-// .map( n => n*2)
-  // .filter(n => n>=10);
+let circle = document.getElementById("circle");
+const source = Observable.fromEvent(document, 'mousemove')
+.map(e => ({x: e.clientX, y: e.clientY}))
+.filter(value => value.x < 500)
+.delay(100);
+
+
+function onNext(value) {
+  circle.style.left = value.x;
+  circle.style.top = value.y;
+}
 
 source.subscribe(
-  value => console.log(value),
+  onNext,
   e => console.log(`error: ${e}`),
   () => console.log("complete")
 );
