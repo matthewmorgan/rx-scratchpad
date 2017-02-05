d4da20a97062d93ad1bffb8caa804e542b0c95be
Matthew Morgan <matt@dulcetsoftware.com>
2017-02-04 20:56:53 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './main',
+  output: { filename: "app.js"},
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js"]
+  }
+}
