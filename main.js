9f40ca825241e5417056bb294ee29942112b5c20
Matthew Morgan <matt@dulcetsoftware.com>
2017-02-05 09:19:31 -0500

    Refactor, use merge'

--- main.js
+++ main.js
@@ -1,56 +1,36 @@
 import {Observable} from "rxjs";
+import {load, loadWithFetch} from "./loader";
 
 
-let button = document.getElementById("button");
-let output = document.getElementById("output");
+let source = Observable.merge(
+        Observable.of(1),
+        Observable.from([2, 3, 4]),
+        Observable.of(5)
+);
 
-const click = Observable.fromEvent(button, 'click');
 
+source.subscribe(
+    value => console.log(`value is ${value}`),
+    error => console.log(`error: ${error}`),
+    () => console.log('complete')
+);
 
-function load(url) {
-  return Observable.create(observer => {
-    let xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", () => {
-      if(xhr.status === 200){
-        let data = JSON.parse(xhr.responseText);
-        observer.next(data);
-        observer.complete();
-      } else {
-        observer.error(xhr.statusText);
-      }
-    });
-
-    xhr.open("GET", url);
-    xhr.send();
-  }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
-}
-
-function loadWithFetch(url){
-  return Observable.defer(()=> Observable.fromPromise(fetch(url).then(r => r.json())));
-}
-
-function retryStrategy({attempts = 4, delay = 1000}){
-  return function(errors){
-    return errors.scan((acc, value) => {
-      console.log(acc, value);
-      return acc + 1;
-    }, 0)
-        .takeWhile(acc => acc < attempts)
-        .delay(delay);
-  }
-}
-
-function renderMovies(movies) {
-  movies.forEach(m => {
-    let div = document.createElement("div");
-    div.innerText = m.title;
-    output.appendChild(div);
-  });
-}
-
-click.flatMap(e => loadWithFetch("movies.json"))
-    .subscribe(
-        renderMovies,
-        e => console.log(`error: ${e}`),
-        () => console.log("complete")
-    );
+// let button = document.getElementById("button");
+// let output = document.getElementById("output");
+//
+// const click = Observable.fromEvent(button, 'click');
+//
+// function renderMovies(movies) {
+//   movies.forEach(m => {
+//     let div = document.createElement("div");
+//     div.innerText = m.title;
+//     output.appendChild(div);
+//   });
+// }
+//
+// click.flatMap(e => loadWithFetch("movies.json"))
+//     .subscribe(
+//         renderMovies,
+//         e => console.log(`error: ${e}`),
+//         () => console.log("complete")
+//     );
