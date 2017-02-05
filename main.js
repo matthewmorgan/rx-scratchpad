da32ba159027e6c3fa4418f29d9904f5788d2eb7
Matthew Morgan <matt@dulcetsoftware.com>
2017-02-05 07:18:29 -0500

    Capture mouse events as Observable

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
-import {Observable} from "rxjs/Observable";
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/filter";
+import {Observable} from "rxjs";
 
-const numbers = [1,5,10];
-const source = Observable.create(observer => {
-  let index = 0;
-  const produceValue = () => {
-    observer.next(numbers[index++]);
-    if (index < numbers.length) {
-      setTimeout(produceValue, 250);
-    } else {
-      observer.complete();
-    }
-  }
 
-  produceValue();
-
-}).map( n => n*2)
-  .filter(n => n>=10);
+const source = Observable.fromEvent(document, 'mousemove');
+// .map( n => n*2)
+  // .filter(n => n>=10);
 
 source.subscribe(
-  value => console.log(`value is: ${value}`),
+  value => console.log(value),
   e => console.log(`error: ${e}`),
   () => console.log("complete")
 );
