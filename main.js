b18cfe183908e9f0a41d8abd1520efdd5d6874b1
Matthew Morgan <matt@dulcetsoftware.com>
2017-02-04 20:59:58 -0500

    Implement concise version

--- main.js
+++ main.js
@@ -3,19 +3,8 @@ import {Observable} from "rxjs";
 let numbers = [1,5,10];
 let source = Observable.from(numbers);
 
-
-class MyObserver {
-  next(value){
-    console.log(`value is: ${value}`);
-  }
-
-  error(e) {
-    console.log(`error: ${e}`);
-  }
-
-  complete() {
-    console.log("complete");
-  }
-}
-
-source.subscribe(new MyObserver());
+source.subscribe(
+  value => console.log(`value is: ${value}`),
+  e => console.log(`error: ${e}`),
+  () => console.log("complete")
+);
